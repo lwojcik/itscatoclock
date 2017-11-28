b98114f016cccc943de7eb21523ab8931a228597
Lukasz Wojcik <kontakt@lukaszwojcik.net>
2017-11-28 22:14:06 +0100

    populated config files and set up environment variables

--- config/twitter.js
+++ config/twitter.js
@@ -1,1 +1,13 @@
-// twitter credentials config
+'use strict'
+
+const env = process.env;
+
+const twitter = {};
+
+twitter.username = env.ICOC_TWITTER_USERNAME;
+twitter.consumerKey = env.ICOC_TWITTER_CONSUMER_KEY; 
+twitter.consumerSecret = env.ICOC_TWITTER_CONSUMER_SECRET;
+twitter.accessToken = env.ICOC_TWITTER_ACCESS_TOKEN;
+twitter.accessTokenSecret = env.ICOC_TWITTER_ACCESS_TOKEN_SECRET;
+
+module.exports = twitter;

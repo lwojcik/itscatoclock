195c9ad4dd9c323521e0f45a9433b39bf5da1ee8
Lukasz Wojcik <kontakt@lukaszwojcik.net>
2017-11-29 22:19:38 +0100

    added database config tests

--- test/config/database.js
+++ test/config/database.js
@@ -1,6 +1,24 @@
+require('dotenv').config();
+const chai = require('chai');
+const expect = chai.expect;
+
+const env = process.env;
+const database = require('../../config/database.js');
+
 describe('Database config', function() {
-  it('should define database object');
-  it('should define database.url property');
-  it('should have non-empty env.ICOC_MONGODB_DB_URL variable');
-  it('database.url should be equal to env.ICOC_MONGODB_DB_URL');
+  it('should define database object', function() {
+    expect(database).to.be.a('object');
+  });
+
+  it('should define database.url property', function() {
+    expect(database).to.have.property('url');
+  });
+ 
+  it('should be able to access non-empty env.ICOC_MONGODB_DB_URL variable', function() {
+    expect(env.ICOC_MONGODB_DB_URL).to.be.a('string');
+  });
+
+  it('database.url should be equal to env.ICOC_MONGODB_DB_URL', function() {
+    expect(database.url).to.equal(env.ICOC_MONGODB_DB_URL);
+  });
 });

d1c900e43d73e8b4bb260e9f70bcb911ae6e970b
Lukasz Wojcik <kontakt@lukaszwojcik.net>
2017-12-08 23:03:40 +0100

    cleaning update file

--- update.js
+++ update.js
@@ -2,46 +2,28 @@
 
 require('dotenv').config();
 
+const Twitter = require('twitter');
+
 const database = require('./config/database.js');
 const app = require('./config/app.js');
 const twitter = require('./config/twitter.js');
 
 const twitterApi = require('./api/twitter.js');
 
-const Twitter = require('twitter');
-
 const banList = require('./models/BanList.js');
 
-const getRandomCatPic = () => {
-  // randomImage
-  // check if the image is in the banlist
-  // if yes, select another
-  // if not, return it
+const places = require('./.places');
 
-  return 'getRandomCatPic';
+const getRandomCatPic = () => {
+
 };
 
 const constructTweet = () => {
-  // get a list of places to figure out the time
 
-  // for place in places
-  // place.name + place.time
-
-  // return string to tweet
-
-  return 'tweetWithTimes';
 }
 
 const startTheMagic = () => {
-  //const randomCatPic = getRandomCatPic();
-  //const tweetWithTimes = constructTweet();
 
-  //twitterApi.postTheTweet(randomCatPic, tweetWithTimes);
-  //console.log(twitter);
-
-  banList.addImage('lol')
-  banList.isImageBanned('lol');
-  banList.purge();
 };
 
 startTheMagic();

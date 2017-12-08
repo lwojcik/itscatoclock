528360a432de34a51e987638d6490c73ed4cd3a3
Lukasz Wojcik <kontakt@lukaszwojcik.net>
2017-12-08 23:15:53 +0100

    adding cached number of images to rotate from

--- config/app.js
+++ config/app.js
@@ -5,5 +5,6 @@ const env = process.env;
 const app = {};
 
 app.imagePath = env.ICOC_IMAGE_PATH;
+app.cachedNumberOfImages = env.ICOC_HOW_MANY_IMAGES_FILE;
 
 module.exports = app;
--- update.js
+++ update.js
@@ -3,24 +3,34 @@
 require('dotenv').config();
 
 const Twitter = require('twitter');
+const fs = require('fs');
+
+// config
 
 const database = require('./config/database.js');
 const app = require('./config/app.js');
 const twitter = require('./config/twitter.js');
 
+// api
+
 const twitterApi = require('./api/twitter.js');
 
-const banList = require('./models/BanList.js');
+// models
 
+const banList = require('./models/BanList.js');
 const places = require('./.places');
 
-const getRandomCatPic = () => {
+// logic
+
+const getRandomCatPic = (next) => {
 
 };
 
 const constructTweet = () => {
 
 }
+
+// main method
 
 const startTheMagic = () => {
 

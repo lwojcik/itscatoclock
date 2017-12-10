ad4ebd23291fc4637b1ff8c1dc04fd4e345084da
Lukasz Wojcik <kontakt@lukaszwojcik.net>
2017-12-10 01:03:06 +0100

    added missing tests

---
+++ test/models/BanList.js
@@ -0,0 +1,22 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const BanList = require('../../models/BanList.js');
+
+describe('BanList Model', function() {
+  it('should define BanList object', function() {
+    expect(BanList).to.be.a('object');
+  });
+
+  it('should expose addImage() method', function() {
+    expect(BanList).to.have.property('addImage').that.is.a('function');
+  });
+
+  it('should expose isImageBanned() method', function() {
+    expect(BanList).to.have.property('isImageBanned').that.is.a('function');
+  });
+
+  it('should expose purge() method', function() {
+    expect(BanList).to.have.property('purge').that.is.a('function');
+  });
+});
